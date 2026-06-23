b6db4ff3f238b272070fbabddfebc083869d8b87
NITISH NAVEEN <nitishnaveen3467@gmail.com>
2026-06-23 15:48:26 +0530

    Add serverExternalPackages to next.config.ts (#25)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.56.1'],
+  serverExternalPackages: ['onnxruntime-node', '@xenova/transformers'],
 };
 
 export default nextConfig;
